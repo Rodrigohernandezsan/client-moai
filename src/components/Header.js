7cf05c32cefb359011972f900e7bc90448b97273
Rodrigohernandezsan <rodrigoherandezsan@gamil.com>
2020-10-22 21:17:21 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,6 +15,9 @@ const Header = () => {
             <div className="header">
               <div className="bounds">
                 <h1 className="header--logo"> 🗿 Moai </h1>
+                <div className="Search">
+                <input className="Search" type="text" placeholder="Search..." aria-label="Search" to="/search"></input>  
+                </div>
                 <nav>
                   <span>Welcome {context.firstName} {context.lastName}</span>
                   <Link className="signout" to="/signout">Sign Out</Link>
