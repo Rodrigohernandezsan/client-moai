cb2ecd7f9764db0ce87cf3300fb2c6c2524913e5
Rodrigohernandezsan <rodrigoherandezsan@gamil.com>
2020-11-12 19:04:12 +0100

    this.state.allCourses

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -56,7 +56,8 @@ export default class Courses extends Component {
     
     //const { srchResults, allCourses } = this.state;4
      
-    console.log("err");  const courseData = this.state.allCourses.filter(course => course.title.toLowerCase().includes(this.state.searchParam.toLowerCase()) ).map(course => {
+    console.log("err,",this.state.allCourses);  
+    const courseData = this.state.allCourses.filter(course => course.title.toLowerCase().includes(this.state.searchParam.toLowerCase()) ).map(course => {
       
       
       return (<div className="grid-33" key={course._id}>
